e0151374483b65847e0b3b94f58d37e81cc66652
szymslow12 <szym.slow12@gmail.com>
2018-04-23 16:10:50 +0200

    add loadXMLDocument in XMLParser, add getFactRepository in FactParser

--- src/main/java/com/codecool/expertsystem/models/FactParser.java
+++ src/main/java/com/codecool/expertsystem/models/FactParser.java
@@ -1,9 +1,20 @@
 package com.codecool.expertsystem.models;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
 public class FactParser extends XMLParser {
 
-    @Override
-    public void loadXMLDocument(String xmlPath) {
-        
+    public FactParser(String xmlPath) {
+        loadXMLDocument(xmlPath);
+    }
+    public void test() {
+        System.out.println("Root element: " + getDoc().getDocumentElement().getNodeName());
+    }
+
+    public getFactRepository() {
+        return new FactRepository(doc);
     }
 }
