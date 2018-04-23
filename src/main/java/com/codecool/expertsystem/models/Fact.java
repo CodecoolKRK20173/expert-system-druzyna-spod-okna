bd8efb043e937a6b2861aeca9fb68169cee00243
szymslow12 <szym.slow12@gmail.com>
2018-04-23 20:18:24 +0200

    implement getFactId/setFactValueById/getValueById/getDescription

--- src/main/java/com/codecool/expertsystem/models/Fact.java
+++ src/main/java/com/codecool/expertsystem/models/Fact.java
@@ -1,30 +1,43 @@
 package com.codecool.expertsystem.models;
 
 import java.util.Set;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Fact {
 
     String id;
     String description;
+    Map<String, Boolean> evalsMap;
 
     public Fact(String id, String description) {
         this.id = id;
         this.description = description;
+        this.evalsMap = new TreeMap<>();
     }
+
+
+    public String getFactId() {
+        return this.id;
+    }
+
 
     public Set<String> getIdSet() {
-        return null;
+        return evalsMap.keySet();
     }
+
 
     public void setFactValueById(String id, boolean value) {
-
+        this.evalsMap.put(id, value);
     }
+
 
     public boolean getValueById(String id) {
-        return true;
+        return evalsMap.get(id);
     }
 
+
     public String getDescription() {
-        return null;
+        return this.description;
     }
 }
