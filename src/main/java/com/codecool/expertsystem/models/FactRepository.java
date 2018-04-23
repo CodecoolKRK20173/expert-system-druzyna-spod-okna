dce5f577f826ea32bf7b5e874222146bc963a6cc
szymslow12 <szym.slow12@gmail.com>
2018-04-23 20:19:23 +0200

    implement addFact

--- src/main/java/com/codecool/expertsystem/models/FactRepository.java
+++ src/main/java/com/codecool/expertsystem/models/FactRepository.java
@@ -1,10 +1,22 @@
 package com.codecool.expertsystem.models;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class FactRepository {
 
-    public void addFact(Fact fact) {
+    private List<Fact> factList;
 
+    public FactRepository() {
+        this.factList = new ArrayList<>();
     }
+
+
+    public void addFact(Fact fact) {
+        this.factList.add(fact);
+    }
+
 
     public Iterator<Fact> getIterator() {
         return null;
