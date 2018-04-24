e7a62a5c6408dd964823f56feceb5f1422f4ebfd
piotrkaminski95 <pkaminki95@gmail.com>
2018-04-24 22:22:35 +0200

    implement answer

--- src/main/java/com/codecool/expertsystem/models/Answer.java
+++ src/main/java/com/codecool/expertsystem/models/Answer.java
@@ -1,14 +1,20 @@
 package com.codecool.expertsystem.models;
 
+import java.util.ArrayList;
+
 public class Answer {
+    ArrayList<Value> values = new ArrayList<Values>();
 
     public boolean evaluateByInput(String input) {
+        
         return true;
     }
 
     public void addValue(Value value) {
-
+        values.add(value);
     }
-
-
+    
+    public ArrayList<Value> getValues() {
+        return values;
+    }
 }
