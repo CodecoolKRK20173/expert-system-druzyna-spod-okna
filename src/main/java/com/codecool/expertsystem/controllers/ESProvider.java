00359184482da41bfaf1ed2d42342ea7899bd38b
WojTechM <wojciech.m1997@gmail.com>
2018-04-24 14:58:00 +0200

    Implement collectAnswers and getAnswerByQuestion methods

--- src/main/java/com/codecool/expertsystem/controllers/ESProvider.java
+++ src/main/java/com/codecool/expertsystem/controllers/ESProvider.java
@@ -1,25 +1,36 @@
 package com.codecool.expertsystem.controllers;
 
 import com.codecool.expertsystem.models.*;
-import com.codecool.expertsystem.views.*;
+import com.codecool.expertsystem.views.UserInterface;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class ESProvider {
 
     private FactRepository factRepository;
     private RuleRepository ruleRepository;
+    private Map<String, Boolean> mapOfAnswers = new LinkedHashMap<>();
 
     public ESProvider(FactParser factParser, RuleParser ruleParser) {
         this.factRepository = factParser.getFactRepository();
         this.ruleRepository = ruleParser.getRuleRepository();
-
     }
 
     public void collectAnswers() {
-
+        QuestionIterator questionIterator = new QuestionIterator(this.ruleRepository);
+        while (questionIterator.hasNext()) {
+            Question nextQuestion = questionIterator.next();
+            String answer = getAnswerByQuestion(nextQuestion);
+            boolean answerAsBoolean = nextQuestion.getAnswer().evaluateByInput(answer);
+            String answerId = nextQuestion.getId();
+            mapOfAnswers.put(answerId, answerAsBoolean);
+        }
     }
 
-    public String getAnswerByQuestion(String questionId) {
-        return "";
+    public String getAnswerByQuestion(Question questionObj) {
+        UserInterface.askQuestion(questionObj.getQuestion());
+        return UserInterface.getAnswer();
     }
 
     public String evaluate() {
