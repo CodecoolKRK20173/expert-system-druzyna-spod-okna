b36e17da729b6b6aeabda057d01f6a1ed80a6bc8
WojTechM <wojciech.m1997@gmail.com>
2018-04-24 13:18:13 +0200

    Implement sample version of UserInterface Asking questions and getting answers

--- src/main/java/com/codecool/expertsystem/views/UserInterface.java
+++ src/main/java/com/codecool/expertsystem/views/UserInterface.java
@@ -2,5 +2,17 @@ package com.codecool.expertsystem.views;
 
 public class UserInterface {
 
+    public static void askQuestion(String question) {
+        printSeparator();
+        System.out.println(question);
+    }
 
+    public static String getAnswer() {
+        System.out.print("Your answer: ");
+        return UserInput.getString();
+    }
+
+    private static void printSeparator() {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
 }
