17d9c6e8ae94994b1728ca4bda032df920652636
WojTechM <wojciech.m1997@gmail.com>
2018-04-24 14:07:12 +0200

    Implement getString method in UserInput class

--- src/main/java/com/codecool/expertsystem/views/UserInput.java
+++ src/main/java/com/codecool/expertsystem/views/UserInput.java
@@ -1,6 +1,12 @@
 package com.codecool.expertsystem.views;
 
+import java.util.Scanner;
+
 public class UserInput {
 
+    public static String getString() {
+        Scanner userInput = new Scanner(System.in);
+        return userInput.next();
+    }
 
 }
