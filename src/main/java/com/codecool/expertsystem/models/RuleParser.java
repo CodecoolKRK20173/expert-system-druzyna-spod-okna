443f109a0624efbf02fc1b9ca89bacb1591eaf73
piotrkaminski95 <pkaminki95@gmail.com>
2018-04-24 22:21:56 +0200

    BugFix: couldn't access some nodes

--- src/main/java/com/codecool/expertsystem/models/RuleParser.java
+++ src/main/java/com/codecool/expertsystem/models/RuleParser.java
@@ -18,6 +18,8 @@ public class RuleParser extends XMLParser {
         RuleRepository ruleRepository = new RuleRepository();
         NodeList nodeList = doc.getElementsByTagName("Rule");
         for (int i = 0; i < nodeList.getLength(); i++) {
+        }
+        for (int i = 0; i < nodeList.getLength(); i++) {
             Element xmlRule = (Element) nodeList.item(i);
             if (xmlRule.getNodeType() == Node.ELEMENT_NODE) {
                 Question parsedQuestion = parseXmlRuleQuestion(xmlRule);
@@ -31,7 +33,6 @@ public class RuleParser extends XMLParser {
         String id = xmlRule.getAttribute("id");
         String questionStr = xmlRule.getElementsByTagName("Question").item(0).getTextContent();
         Answer answer = parseXmlRuleAnswer((Element) xmlRule.getElementsByTagName("Answer").item(0));
-
         return new Question(id, questionStr, answer);
     }
 
@@ -40,15 +41,20 @@ public class RuleParser extends XMLParser {
         Answer answer = new Answer();
 
         for (int i = 0; i < selectionList.getLength(); i++) {
-            Node node = selectionList.item(i);
-            if (node.getNodeName().equals("SingleValue")) {
-                System.out.println("SingleValue node");
-                answer.addValue(new SingleValue(node.getChildNodes().item(0).getNodeValue(), (node.getNodeValue().equals("true")?true:false)));
-            } else if (node.getNodeName().equals("MultipleValue")) {
-                System.out.println("MultipleValue");
-                String[] array = node.getChildNodes().item(0).getNodeValue().split(",");
-                ArrayList<String> valueList = new ArrayList<String>(Arrays.asList(array));
-                answer.addValue(new MultipleValue(valueList, (node.getNodeValue().equals("true")?true:false)));
+            NodeList childList = selectionList.item(i).getChildNodes();
+            for (int j = 0; j < childList.getLength(); j++) {
+                if (childList.item(j) instanceof Element) {
+                    Element child = (Element) childList.item(j);
+                    Element parent = (Element) selectionList.item(i);
+                    String attrName = "value";
+
+                    if (child.getTagName().equals("SingleValue")) {
+                        answer.addValue(new SingleValue(child.getAttribute(attrName), (parent.getAttribute(attrName).equals("true")?true:false)));
+                    } else if (child.getTagName().equals("MultipleValue")) {
+                        ArrayList<String> valueList = new ArrayList<String>(Arrays.asList(child.getAttribute(attrName).split(",")));
+                        answer.addValue(new MultipleValue(valueList, (parent.getAttribute(attrName).equals("true")?true:false)));
+                    }
+                }
             }
         }
         return answer;
