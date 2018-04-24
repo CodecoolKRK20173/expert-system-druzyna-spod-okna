8032d464a41783d221a913bca99131dae6a8ba0c
Senseinvader <d.ivantsov1983@gmail.com>
2018-04-24 15:02:17 +0200

    add method to split usr input inAnswer class

--- src/main/java/com/codecool/expertsystem/models/Answer.java
+++ src/main/java/com/codecool/expertsystem/models/Answer.java
@@ -1,14 +1,26 @@
 package com.codecool.expertsystem.models;
 
+import java.util.List;
+import java.util.Arrays;
+
 public class Answer {
+    List<Value> values;
+    List<String> splittedInputList;
 
     public boolean evaluateByInput(String input) {
+        splittedInputList = splitString(input);
+
         return true;
     }
 
     public void addValue(Value value) {
 
     }
+
+    private List<String> splitString(String input) {
+        return Arrays.asList(input.split(","));
+        }
+
 
 
 }
