4ce2d1e495a41800b35a98d279fa4b5e9c9d0dec
szymslow12 <szym.slow12@gmail.com>
2018-04-24 10:38:38 +0200

    implement getInputPattern/getSelectionType in Value

--- src/main/java/com/codecool/expertsystem/models/Value.java
+++ src/main/java/com/codecool/expertsystem/models/Value.java
@@ -1,15 +1,25 @@
 package com.codecool.expertsystem.models;
 
 import java.util.List;
+import java.util.ArrayList;
 
 
 public abstract class Value {
 
     public List<String> getInputPattern() {
-        return null;
+        if (this instanceof SingleValue) {
+            List<String> inputPattern = new ArrayList<>();
+            inputPattern.add(((SingleValue) this).getParam());
+            return inputPattern;
+        } else {
+            return ((MultipleValue) this).getParam();
+        }
     }
 
     public boolean getSelectionType() {
-        return true;
+        if (this instanceof SingleValue)
+            return ((SingleValue) this).getSelectionType();
+        else
+            return ((MultipleValue) this).getSelectionType();
     }
 }
