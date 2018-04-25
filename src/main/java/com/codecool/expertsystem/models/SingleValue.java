73ecdd70f83cfc0f73d29da09df84a197e16374f
piotrkaminski95 <pkaminki95@gmail.com>
2018-04-25 11:36:37 +0200

    chenge fields visibility to private

--- src/main/java/com/codecool/expertsystem/models/SingleValue.java
+++ src/main/java/com/codecool/expertsystem/models/SingleValue.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class SingleValue extends Value {
 
-    String param;
-    boolean selectionType;
+    private String param;
+    private boolean selectionType;
 
     public SingleValue(String param, boolean selectionType) {
         this.param = param;
