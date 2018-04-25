d5012cda77001e866583c6cd1ad5fb10f1a4d480
piotrkaminski95 <pkaminki95@gmail.com>
2018-04-25 09:53:22 +0200

    refactor

--- src/main/java/com/codecool/expertsystem/models/RuleParser.java
+++ src/main/java/com/codecool/expertsystem/models/RuleParser.java
@@ -17,8 +17,7 @@ public class RuleParser extends XMLParser {
     public RuleRepository getRuleRepository() {
         RuleRepository ruleRepository = new RuleRepository();
         NodeList nodeList = doc.getElementsByTagName("Rule");
-        for (int i = 0; i < nodeList.getLength(); i++) {
-        }
+
         for (int i = 0; i < nodeList.getLength(); i++) {
             Element xmlRule = (Element) nodeList.item(i);
             if (xmlRule.getNodeType() == Node.ELEMENT_NODE) {
@@ -41,22 +40,31 @@ public class RuleParser extends XMLParser {
         Answer answer = new Answer();
 
         for (int i = 0; i < selectionList.getLength(); i++) {
-            NodeList childList = selectionList.item(i).getChildNodes();
-            for (int j = 0; j < childList.getLength(); j++) {
-                if (childList.item(j) instanceof Element) {
-                    Element child = (Element) childList.item(j);
-                    Element parent = (Element) selectionList.item(i);
-                    String attrName = "value";
-
-                    if (child.getTagName().equals("SingleValue")) {
-                        answer.addValue(new SingleValue(child.getAttribute(attrName), (parent.getAttribute(attrName).equals("true")?true:false)));
-                    } else if (child.getTagName().equals("MultipleValue")) {
-                        ArrayList<String> valueList = new ArrayList<String>(Arrays.asList(child.getAttribute(attrName).split(",")));
-                        answer.addValue(new MultipleValue(valueList, (parent.getAttribute(attrName).equals("true")?true:false)));
-                    }
-                }
-            }
+            answer.addValue(getValueObj(selectionList.item(i)));
         }
         return answer;
+    }
+
+    private Value getValueObj(Node selection) {
+        NodeList childList = selection.getChildNodes();
+        for (int j = 0; j < childList.getLength(); j++) {
+            if (childList.item(j) instanceof Element) {
+                return getValueInstance((Element)childList.item(j), (Element)selection);
+            }
+        }
+        return null;
+    }
+
+    private Value getValueInstance(Element child, Element parent) {
+        String attrName = "value";
+
+        if (child.getTagName().equals("SingleValue")) {
+            return new SingleValue(child.getAttribute(attrName), (parent.getAttribute(attrName).equals("true")?true:false));        
+        } else if (child.getTagName().equals("MultipleValue")) {
+            ArrayList<String> valueList = new ArrayList<String>(Arrays.asList(child.getAttribute(attrName).split(",")));
+            return new MultipleValue(valueList, (parent.getAttribute(attrName).equals("true")?true:false));            
+        } else {
+            return null;
+        }
     }
 }
