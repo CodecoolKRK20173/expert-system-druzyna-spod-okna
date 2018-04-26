16c0ed20ad82554ed99af0345ed9ef02a416c012
Senseinvader <d.ivantsov1983@gmail.com>
2018-04-26 13:56:20 +0200

    resolve conflicts merge

--- src/main/java/com/codecool/expertsystem/models/RuleRepository.java
+++ src/main/java/com/codecool/expertsystem/models/RuleRepository.java
@@ -4,6 +4,15 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * <h1>RuleRepository</h1>
+ * The RuleRepository allows to form the collection of questions and
+ * to operate it by adding, deleting elements and iterate over them.
+* <p>
+ * stores Question objects
+ * @author pkaminski
+ * @version 1.0
+ */
 public class RuleRepository {
 
     List<Question> questions = new ArrayList<Question>();
