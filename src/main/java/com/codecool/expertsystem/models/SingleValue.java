9c00a0ef8616b7d1fc78effd299db4512d8dc0ad
piotrkaminski95 <pkaminki95@gmail.com>
2018-04-26 11:09:08 +0200

    add docSting

--- src/main/java/com/codecool/expertsystem/models/SingleValue.java
+++ src/main/java/com/codecool/expertsystem/models/SingleValue.java
@@ -3,11 +3,23 @@ package com.codecool.expertsystem.models;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <h1>SingleValue</h1>
+ * stores acceptable answers
+ * @author pkaminski
+ * @version 1.0
+ */
 public class SingleValue extends Value {
 
     private String param;
     private boolean selectionType;
 
+    /**
+     * Constructor
+     *  sets fields with given parameters
+     *  @param param acceptable answer
+     *  @param selectionType 
+     */
     public SingleValue(String param, boolean selectionType) {
         this.param = param;
         this.selectionType = selectionType;
