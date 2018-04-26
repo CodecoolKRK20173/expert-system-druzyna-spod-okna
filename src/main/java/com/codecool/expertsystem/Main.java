20415d5fff7d0670a61e4c8895284f29ce5fc579
WojTechM <35366448+WojTechM@users.noreply.github.com>
2018-04-26 09:35:39 +0200

    Implemented Main class (#4)
    + minor fixes in Answer

--- src/main/java/com/codecool/expertsystem/Main.java
+++ src/main/java/com/codecool/expertsystem/Main.java
@@ -1,7 +1,15 @@
 package com.codecool.expertsystem;
 
+import com.codecool.expertsystem.controllers.ESProvider;
+import com.codecool.expertsystem.models.FactParser;
+import com.codecool.expertsystem.models.RuleParser;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        FactParser factParser = new FactParser("src/main/resources/oncologistDiagnose.xml");
+        RuleParser ruleParser = new RuleParser("src/main/resources/oncologistQuestions.xml");
+        new ESProvider(factParser, ruleParser);
     }
 }
